42bc7ee199d5571c5b87a26681da46af93353fa9
Alex <alexdev.gc@gmail.com>
2022-05-10 17:05:18 -0500

    newCommit few changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,34 +1,36 @@
 import React from 'react'
 
 const About = () => {
-  return (
-    <div 
-    name='about'
-    className='w-full h-screen bg-slate-200 text-[#14274E]'>
-        <div className='flex flex-col justify-center items-center w-full h-full'>
-            <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
-                <div className='sm:text-right pb-8 pl-4'>
-                    <p className='text-4xl font-bold inline border-b-4 border-[#00a6ff]'>
-                        About
-                    </p>
+    return (
+        <div
+            name='about'
+            className='w-full h-screen bg-slate-200 text-[#14274E]'>
+            <div className='flex flex-col justify-center items-center w-full h-full'>
+                <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
+                    <div className='sm:text-right pb-8 pl-4'>
+                        <p className='text-4xl font-bold inline border-b-4 border-[#00a6ff]'>
+                            About
+                        </p>
+                    </div>
+                </div>
+                <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8 px-4'>
+                    <div className='sm:text-right text-4xl font-bold'>
+                        <p>Hi. I'm Alex, welcome, please take a look around!</p>
+                    </div>
+                    <div>
+                        <p>
+                            Self taught web developer, but also an Economist,<br />
+                            currently based in Cali, Colombia.<br />
+
+                            I really enjoy sports, outdoor activities and get
+                            in touch with interesting people who likes to share their ideas in
+                            different areas of knowledge.
+                        </p>
+                    </div>
                 </div>
             </div>
-            <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8 px-4'>
-                <div className='sm:text-right text-4xl font-bold'>
-                    <p>Hi. I'm Alex, welcome, please take a look around!</p>
-                </div>
-                <div>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                        Qui aspernatur ea aliquam, distinctio culpa et, minima 
-                        maiores sapiente magni mollitia omnis repellendus! Dolore 
-                        laudantium reprehenderit odit, perferendis deleniti sint velit 
-                        facere molestiae fugit vitae aspernatur vero iusto, laborum, 
-                        cumque obcaecati!</p>
-                </div>
-            </div>
-        </div>        
-    </div>
-  )
+        </div>
+    )
 }
 
 export default About
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,28 +1,30 @@
 import React from 'react'
-import {Link} from 'react-scroll'
+import { Link } from 'react-scroll'
 
 const Home = () => {
-  return (
-    <div name='home' className='w-full h-screen bg-slate-200'>
-        {/* container */}
-        <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
-            <p className=' text-[#00a6ff] text-2xl'>Hello, my name is</p>
-            <h1 className='text-4xl sm:text-7xl font-bold text-[#002b78]'>Alex</h1>
-            <h2 className='text-[#14274E] text-4xl'>I'm a web developer.</h2>
-            <p className='text-[#14274E] py-4 max-w-[700px]'>
-                
-            </p>
-            <form>
-                <button className='text-[#14274E] font-bold border-2 border-[#14274E] px-6 py-3 my-2 flex flex-row items-center hover:bg-[#002b78] hover:text-slate-200'>
-                    <Link to='work' smooth={true} duration={500}>
-                        View my work 
-                    </Link>
-                </button>
-            </form>
-        </div>
+    return (
+        <div name='home' className='w-full h-screen bg-slate-200'>
+            {/* container */}
+            <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+                <p className=' text-[#00a6ff] text-2xl'>Hello, my name is</p>
+                <h1 className='text-4xl sm:text-7xl font-bold text-[#002b78]'>Alex</h1>
+                <h2 className='text-[#14274E] text-4xl'>I'm a web developer.</h2>
+                <p className='text-[#14274E] py-4 max-w-[700px]'>
+                    Passionate about technology and always looking for new challenges,
+                    in this portfolio I'll show you my skill set, focused on modern JavaScript with all the technologies around it,
+                    and a few things about me.
+                </p>
+                <form>
+                    <button className='text-[#14274E] font-bold border-2 border-[#14274E] px-6 py-3 my-2 flex flex-row items-center hover:bg-[#002b78] hover:text-slate-200'>
+                        <Link to='work' smooth={true} duration={500}>
+                            View my work
+                        </Link>
+                    </button>
+                </form>
+            </div>
 
-    </div>
-  )
+        </div>
+    )
 }
 
 export default Home
